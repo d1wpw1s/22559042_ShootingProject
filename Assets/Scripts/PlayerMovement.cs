88136d4a711b1e91075ef000c8b86ed30eede5f5
d1wpw1s <t01089158176@gmail.com>
2026-03-18 11:11:03 +0900

    3주차 프로젝트 복사본
    20260318 생성
    프로젝트 처음 복사본

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float spd = 5;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        float v = Input.GetAxis("Vertical");
+
+        //Vector3 direct = Vector3,right * h + Vector3.up * v;
+        Vector3 direct = new Vector3(h, v, 0);
+
+
+        //transform.Translate(direct + spd * Time.deltaTime);
+        transform.position = transform.position + direct * spd *Time.deltaTime;
+    }
+}
